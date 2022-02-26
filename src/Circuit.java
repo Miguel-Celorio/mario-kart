5fe4745c55b8e6917b4bfbbc278797ccecec2115
Miguel <miguel.celorio.09@gmail.com>
2022-02-26 17:07:10 +0100

    Clase Circuit

---
+++ src/Circuit.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+public class Circuit {
+	private String name;
+	private int distance;
+	
+	public Circuit(String name, int distance) {
+		super();
+		this.name = name;
+		this.distance = distance;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+	public int difficulty() {
+		Random random = new Random();
+		return random.nextInt(5);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Circuit [name=");
+		builder.append(name);
+		builder.append(", distance=");
+		builder.append(distance);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+}
